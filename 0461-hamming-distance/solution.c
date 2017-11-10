bfb08d850d9f9b07c7df92ea9ff76026b39d0fc3
Chu Wang <heromanba24@gmail.com>
2017-11-10 01:48:32 +0000

    [LeetCode Sync] Runtime - 3 ms (0.58%), Memory - N/A (N/A)

---
+++ 0461-hamming-distance/solution.c
@@ -0,0 +1,12 @@
+int hammingDistance(int x, int y) {
+    
+    int distance = 0;
+    int z = x ^ y;
+    while ( z != 0) {
+        if (z % 2 == 1){
+            distance++;
+        }
+        z = z / 2;
+    }
+    return distance;
+}
