ce31fad16b8469a6e7012fb8d44407854f530852
Chu Wang <heromanba24@gmail.com>
2020-09-03 02:09:46 +0000

    [LeetCode Sync] Runtime - 88 ms (2.29%), Memory - 16.1 MB (100.00%)

---
+++ 0108-convert-sorted-array-to-binary-search-tree/solution.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        def convert(nums, left, right):
+            if left > right:
+                return None
+            mid = (left + right) // 2
+            root = TreeNode(nums[mid])
+            root.left = convert(nums, left, mid - 1)
+            root.right = convert(nums, mid + 1, right)
+            return root
+        return convert(nums, 0, len(nums) - 1)
+            
