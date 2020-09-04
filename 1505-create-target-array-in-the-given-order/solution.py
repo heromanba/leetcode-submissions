6b1b2bb93f08bc093f0cf883d1f8ea464fb64bc8
Chu Wang <heromanba24@gmail.com>
2020-09-04 08:04:02 +0000

    [LeetCode Sync] Runtime - 36 ms (0.28%), Memory - 13.8 MB (100.00%)

---
+++ 1505-create-target-array-in-the-given-order/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        res = []
+        for num, idx in zip(nums, index):
+            res.insert(idx, num)
+        return res
