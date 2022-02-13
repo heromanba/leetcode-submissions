e3bcac7f01960911ba6af462426058357b35459a
Chu Wang <heromanba24@gmail.com>
2022-02-13 07:57:08 +0000

    [LeetCode Sync] Runtime - 41 ms (0.09%), Memory - 13.9 MB (100.00%)

--- 0017-letter-combinations-of-a-phone-number/solution.py
+++ 0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,26 +1,24 @@
 class Solution:
-    mapping = {
-        "2": ["a", "b", "c"],
-        "3": ["d", "e", "f"],
-        "4": ["g", "h", "i"],
-        "5": ["j", "k", "l"],
-        "6": ["m", "n", "o"],
-        "7": ["p", "q", "r", "s"],
-        "8": ["t", "u", "v"],
-        "9": ["w", "x", "y", "z"],
-    }
-    
     def letterCombinations(self, digits: str) -> List[str]:
-        
+        mapping = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
+        }
+        results = []
         if len(digits) == 0:
-            return []
-        if len(digits) == 1:
-            return self.mapping[digits]
-        combined = []
-        for c in self.mapping[digits[0]]:
-            for ac in self.letterCombinations(digits[1:]):
-                combined.append(c + ac)
-        return combined
-    
-        
+            return results
+        def backtrack(results, current_str, current_pos, digits):
+            if current_pos == len(digits):
+                results.append(current_str)
+                return
+            for c in mapping[digits[current_pos]]:
+                backtrack(results, current_str + c, current_pos + 1, digits)
+        backtrack(results, "", 0, digits)
+        return results
         
