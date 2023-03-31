f903664fe785f821f401eedf086e6951ccc7b5e1
Chu Wang <heromanba24@gmail.com>
2023-03-31 02:31:18 +0000

    [LeetCode Sync] Runtime - 51 ms (5.63%), Memory - 18.9 MB (100.00%)

---
+++ 0110-balanced-binary-tree/solution.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        ret, dep = self.isBalancedAndDepth(root)
+        return ret
+
+    def isBalancedAndDepth(self, node):
+        if not node:
+            return True, 0
+        # left child
+        isLeftBalanced, leftDepth = self.isBalancedAndDepth(node.left)
+        # right child
+        isRightBalanced, rightDepth = self.isBalancedAndDepth(node.right)
+        return (isLeftBalanced and isRightBalanced) and abs(leftDepth - rightDepth) <= 1, max(leftDepth, rightDepth) + 1
