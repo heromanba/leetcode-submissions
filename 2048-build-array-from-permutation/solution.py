40acb41805cd968daeb50e614852b952e6a5d9bb
Chu Wang <heromanba24@gmail.com>
2023-04-08 23:45:44 +0000

    [LeetCode Sync] Runtime - 117 ms (6.81%), Memory - 14.1 MB (100.00%)

---
+++ 2048-build-array-from-permutation/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ret = []
+        for i in range(len(nums)):
+            ret.append(nums[nums[i]])
+        return ret
