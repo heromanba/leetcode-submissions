b124fbe46c48b29b4d0670bc5ca484798b353ab3
Chu Wang <heromanba24@gmail.com>
2023-04-09 00:34:08 +0000

    [LeetCode Sync] Runtime - 52 ms (6.03%), Memory - 13.9 MB (100.00%)

---
+++ 1651-shuffle-string/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        ret = [None] * len(s)
+        for i, c in zip(indices, s):
+            ret[i] = c
+        return "".join(ret)
