0fc138fe0fbfeb5422166cc15854a471f82f4ac9
Chu Wang <heromanba24@gmail.com>
2023-04-15 06:40:04 +0000

    [LeetCode Sync] Runtime - 74 ms (5.46%), Memory - 15.5 MB (100.00%)

--- 0617-merge-two-binary-trees/solution.py
+++ 0617-merge-two-binary-trees/solution.py
@@ -1,3 +1,10 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
     def mergeTrees(self, t1, t2):
         """
@@ -5,24 +12,11 @@ class Solution:
         :type t2: TreeNode
         :rtype: TreeNode
         """
-        """
-        ###C Implementation###
-        Definition for a binary tree node.
-        struct TreeNode {
-        int val;
-        struct TreeNode *left;
-        struct TreeNode *right;
-        };
-        """
-        if (t1 is not None) and (t2 is not None):
-            t1.val = t1.val + t2.val
-            t1.left = self.mergeTrees(t1.left, t2.left)
-            t1.right = self.mergeTrees(t1.right, t2.right)
-        if (t1 is not None) and (t2 is None):
-            pass
-        if (t1 is None) and (t2 is not None):
-            t1 = t2
-        if (t1 is None) and (t2 is None):
-            pass
+        if not t1:
+            return t2
+        if not t2:
+            return t1
+        t1.val = t1.val + t2.val
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
         return t1
-            
