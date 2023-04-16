b70ae9c43c63d4f3604d0d834d3cca9dcaad3950
Chu Wang <heromanba24@gmail.com>
2023-04-16 01:30:58 +0000

    [LeetCode Sync] Runtime - 89 ms (5.97%), Memory - 19 MB (100.00%)

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/solution.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/solution.py
@@ -7,10 +7,34 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        while (root.val - p.val) * (root.val - q.val) > 0:
-            if root.val - p.val > 0:
-                root = root.left
-            else:
-                root = root.right
-        return root
+        traversal = []
+        heights = []
+        def dfs(node, curr_h):
+            if not node:
+                return
+            traversal.append(node)
+            heights.append(curr_h)
+            
+            if node.left:
+                dfs(node.left, curr_h+1)
                 
+                traversal.append(node)
+                heights.append(curr_h)
+
+            if node.right:    
+                dfs(node.right, curr_h+1)
+            
+        dfs(root, 0)
+        
+        p_idx = traversal.index(p)
+        q_idx = traversal.index(q)
+
+        min_h = float('inf')
+        min_h_node = None
+        for i in range(min(p_idx, q_idx), max(p_idx, q_idx)):
+            if heights[i] < min_h:
+                min_h = heights[i]
+                min_h_node = traversal[i]
+                
+        return min_h_node
+
