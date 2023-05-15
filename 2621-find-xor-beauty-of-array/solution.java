29a0749920dc1cefa0eb1d43838257af20d0cd42
Chu Wang <heromanba24@gmail.com>
2023-05-15 13:59:38 +0000

    [LeetCode Sync] Runtime - 1 ms (96.62%), Memory - 59.1 MB (100.00%)

---
+++ 2621-find-xor-beauty-of-array/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int xorBeauty(int[] nums) {
+        int xor = 0;
+        for(int i=0; i<nums.length; i++) {
+            xor ^= nums[i];
+        }
+        return xor;
+    }
+}
