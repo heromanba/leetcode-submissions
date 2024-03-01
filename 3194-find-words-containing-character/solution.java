d690e93fe411dcb26eece8c0c56c5bf53b6d4be4
Chu Wang <heromanba24@gmail.com>
2024-03-01 13:43:13 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 44.6 MB (99.95%)

---
+++ 3194-find-words-containing-character/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> ret = new ArrayList<>();
+        for (int i=0; i<words.length; i++) {
+            if (words[i].indexOf(x) != -1)
+                ret.add(i);
+        }
+        return ret;
+    }
+}
