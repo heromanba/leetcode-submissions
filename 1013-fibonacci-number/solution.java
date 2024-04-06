d816a1dfd124fb92bb6c0b42e2cdc52735f65df0
Chu Wang <heromanba24@gmail.com>
2024-04-06 14:11:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.4 MB (99.99%)

---
+++ 1013-fibonacci-number/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    private int[] mem = null;
+    
+    public int fib(int n) {
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        mem = new int[n+1];
+        mem[0] = 0;
+        mem[1] = 1;
+        for(int i=2; i<=n; i++) {
+            mem[i] = mem[i-1] + mem[i-2];
+        }
+        return mem[n];
+    }
+}
