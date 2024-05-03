d652584802dbd9d822f2bce8494c474da03a42e7
Chu Wang <heromanba24@gmail.com>
2024-05-03 13:49:57 +0000

    [LeetCode Sync] Runtime - 35 ms (76.53%), Memory - 54.8 MB (99.99%)

--- 0215-kth-largest-element-in-an-array/solution.java
+++ 0215-kth-largest-element-in-an-array/solution.java
@@ -1,38 +1,71 @@
 class Solution {
+    
+    private Integer[] heap;
+    private int N = 0;
+    
+    private void exch(int i, int j) {
+        int tmp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = tmp;
+    }
+    
+    private void delMin() {
+        heap[0] = heap[--N];
+        heap[N] = null;
+        sink(0);
+    }
+    
+    private void sink(int i) {
+        while(i < N) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int child;
+            if ((right <= N-1) && (heap[right] < heap[left]))
+                child = right;
+            else if (left <= N-1)
+                child = left;
+            else
+                break;
+            if(heap[child] < heap[i]) {
+                exch(i, child);
+                i = child;
+            } else
+                break;
+        }
+    }
+    
+    private void swim(int i) {
+        while(i >= 0) {
+            int parent = (i+1)/2-1;
+            if((parent>=0)&&(heap[parent] > heap[i])) {
+                exch(i, parent);
+                i = parent;
+            }
+            else
+                break;
+        }
+    }
+    
+    private void insert(int i) {
+        heap[N++] = i;
+        swim(N-1);
+    }
+    
     public int findKthLargest(int[] nums, int k) {
-        int l = 0, h = nums.length - 1;
-        k = nums.length - k;
-        while(true) {
-            int p = part(nums, l, h);
-            if (p == k) break;
-            else if (p < k) {
-                l = p + 1;
-            } else {
-                h = p - 1;
+        heap = new Integer[k];
+        for(int i: nums) {
+            if(N < k)
+                insert(i);
+            else {
+                if(i > heap[0]) {
+                    delMin();
+                    insert(i);
+                }
             }
+            // List<Integer> list = Arrays.asList(heap);
+            // System.out.println("\ni:"+i+"N:"+N);
+            // System.out.println(list);
         }
-        
-        return nums[k];
-    }
-    
-    private int part(int[] nums, int l, int r) {
-        swap(nums, (l + r) / 2, r);
-        int p = r--;
-        while(l <= r) {
-            if (nums[l] >= nums[p]) {
-                swap(nums, l, r--);
-            } else {
-                l++;
-            }
-        }
-        
-        swap(nums, l, p);
-        return l;
-    }
-    
-    private void swap (int[] nums, int left, int right) {
-            int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
+        return heap[0];
     }
 }
