821cc5aa33e438913e9a06a34d33efbd6d57a097
Chu Wang <heromanba24@gmail.com>
2026-05-01 05:47:07 +0000

    [LeetCode Sync] Runtime - 183 ms (5.41%), Memory - 29.8 MB (7.21%)

--- 0121-best-time-to-buy-and-sell-stock/solution.py
+++ 0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,16 +1,19 @@
 class Solution:
-
     def maxProfit(self, prices: List[int]) -> int:
-        maxList = [None] * len(prices)
-        # find the max of suffix
-        for i in range(1, len(prices)+1):
-            if i == 1:
-                maxList[-1] = prices[-1]
-            else:
-                maxList[-i] = max(prices[-i], maxList[-i+1])
-        ret = 0
+        prefix_min_list = [None] * len(prices)
+        postfix_max_list = [None] * len(prices)
         for i in range(len(prices)):
-            if maxList[i] - prices[i] > ret:
-                ret = maxList[i] - prices[i]
-        return ret
+            if i == 0:
+                prefix_min_list[i] = prices[i]
+            else:
+                prefix_min_list[i] = min(prefix_min_list[i-1], prices[i])
+        for i in range(len(prices)-1, -1, -1):
+            if i == len(prices)-1:
+                postfix_max_list[i] = prices[i]
+            else:
+                postfix_max_list[i] = max(postfix_max_list[i+1], prices[i])
+        max_delta = -1
+        for i in range(len(prices)):
+            max_delta = max(max_delta, postfix_max_list[i]-prefix_min_list[i])
+        return max_delta
 
